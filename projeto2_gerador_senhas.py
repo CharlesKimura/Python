39759193dd9883d7905161d0b03cf97e1987d581
Charles <dalacrox@gmail.com>
2025-07-12 09:09:18 -0300

    Gerador de senha

--- projeto2_gerador_senhas.py
+++ projeto2_gerador_senhas.py
@@ -6,8 +6,11 @@ numeros = '0123456789'
 simbolos = '!@#$%*'
 tamanho = int(input('Quantos caracteres sua senha dever ter? '))
 caracteres = minusculas + maiusculas + numeros + simbolos
-senha = ''
 
-for i in range(tamanho):
-    senha += choice(caracteres)
-print(f'Senha gerada: {senha}')
+senha = ''
+if tamanho <= 7:
+    print('Erro, tamanho deve ser maior que 7')
+else:
+    for i in range(tamanho):
+        senha += choice(caracteres)
+    print(f'Senha gerada: {senha}')
